c50d567331473c96e4c32c0816d6d6789c0172a6
SergWebAcceptance <serg.web@acceptance.ltd>
2024-10-22 22:51:37 +0300

    added uploads plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,17 @@
 module.exports = () => ({
-    "users-permissions": {
-        config: {
-        register: {
-            // put the name of your added fields here
-            allowedFields: ["firstName", "lastName", "phone"],
-        },
-        },
-    }
+  "users-permissions": {
+    config: {
+      register: {
+        // put the name of your added fields here
+        allowedFields: ["firstName", "lastName", "phone"],
+      },
+    },
+  },
+  upload: {
+    provider: "local",
+    providerOptions: {
+      sizeLimit: 1000000, // Adjust this limit if needed (in bytes)
+      path: "/var/data", // Use the mount path from Render
+    },
+  },
 });
