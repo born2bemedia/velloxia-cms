e8907ad92f6c01210cc3fa63396e20983e8e6604
SergWebAcceptance <serg.web@acceptance.ltd>
2024-10-22 22:09:20 +0300

    added fields

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = () => ({
+    "users-permissions": {
+        config: {
+        register: {
+            // put the name of your added fields here
+            allowedFields: ["firstName", "firstName", "phone"],
+        },
+        },
+    }
+});
