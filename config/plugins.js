7ff3ba52f986c73b47a3cb8728964d897c080cf2
SergWebAcceptance <serg.web@acceptance.ltd>
2024-10-25 16:01:52 +0300

    Added smtp

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,24 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        secure: env("SMTP_SECURE", false),
+      },
+      settings: {
+        defaultFrom: "noreply@velloxia.com",
+        defaultReplyTo: "noreply@velloxia.com",
+      },
+    },
+  },
   upload: {
     config: {
       provider: 'cloudinary',
